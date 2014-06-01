65fedb9ea217134a842b7b34cf425c24ca6ca3b7
Alan Coopersmith <alan.coopersmith@oracle.com>
2014-05-31 21:39:32 -0700

    autogen.sh: Honor NOCONFIGURE=1
    See http://people.gnome.org/~walters/docs/build-api.txt
    
    Also includes support for builds outside $srcdir as found in most
    other X.Org autogen.sh files.
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
 autoreconf -v --install || exit 1
-./configure "$@"
+cd $ORIGDIR || exit $?
+
+if test -z "$NOCONFIGURE"; then
+    $srcdir/configure "$@"
+fi
